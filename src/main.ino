967558eccefbb0daeba4cd0d496de6076bf2a0a4
Pablo Cardozo <pablocyc83@gmail.com>
2020-01-03 18:23:00 -0400

    Controlando los leds con un array

--- src/main.ino
+++ src/main.ino
@@ -2,6 +2,12 @@
 #define pinLedVerde   10
 #define pinLedAzul     9
 
+int pinLeds[] = {
+  pinLedRojo,
+  pinLedVerde,
+  pinLedAzul
+};
+
 void setup () {
   pinMode(pinLedRojo, OUTPUT);
   pinMode(pinLedVerde, OUTPUT);
@@ -10,11 +16,11 @@ void setup () {
 
 void loop () {
   for (int i=0; i<=255; i++) {
-    analogWrite(pinLedRojo, i);
+    analogWrite(pinLeds[1], i);
     delay(5);
   }
   for (int i=255; i>=0; i--) {
-    analogWrite(pinLedRojo, i);
+    analogWrite(pinLeds[1], i);
     delay(5);
   }
 }
