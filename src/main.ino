345e46ac58fb29bdec3f90dd7acbce593f025d15
Pablo Cardozo <pablocyc83@gmail.com>
2020-01-03 18:37:49 -0400

    Controlando los leds con un pulsador

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,7 @@
 #define pinLedRojo    11
 #define pinLedVerde   10
 #define pinLedAzul     9
+#define pinButton      2
 
 int pinLeds[] = {
   pinLedRojo,
@@ -8,19 +9,40 @@ int pinLeds[] = {
   pinLedAzul
 };
 
+int contLed;
+
 void setup () {
   pinMode(pinLedRojo, OUTPUT);
   pinMode(pinLedVerde, OUTPUT);
   pinMode(pinLedAzul, OUTPUT);
+  pinMode(pinButton, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(pinButton), rotarLed, FALLING);
+
+  off();
 }
 
 void loop () {
   for (int i=0; i<=255; i++) {
-    analogWrite(pinLeds[1], i);
+    analogWrite(pinLeds[contLed], i);
     delay(5);
   }
   for (int i=255; i>=0; i--) {
-    analogWrite(pinLeds[1], i);
+    analogWrite(pinLeds[contLed], i);
     delay(5);
   }
+  attachInterrupt(digitalPinToInterrupt(pinButton), rotarLed, FALLING);
+}
+
+void rotarLed () {
+  detachInterrupt(digitalPinToInterrupt(pinButton));
+  contLed++;
+  if (contLed > 2)
+    contLed = 0;
+}
+
+void off () {
+  analogWrite(pinLedRojo, 0);
+  analogWrite(pinLedVerde, 0);
+  analogWrite(pinLedAzul, 0);
 }
